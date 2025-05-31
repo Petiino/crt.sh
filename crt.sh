f30389569595af0ea2186e0b695458db3afefbd3
Pétino <62351780+Petiino@users.noreply.github.com>
2025-05-31 15:54:34 +0200

    Change the output directory with the curent directory

--- crt.sh
+++ crt.sh
@@ -24,26 +24,26 @@ echo "
 	requestsearch="$(curl -s "https://crt.sh?q=%.$req&output=json")"
 		 
 			 echo $requestsearch > req.txt
-			 cat req.txt | jq ".[].common_name,.[].name_value"| cut -d'"' -f2 | sed 's/\\n/\n/g' | sed 's/\*.//g'| sed -r 's/([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})//g' |sort | uniq > output/domain.$req.txt
+			 cat req.txt | jq ".[].common_name,.[].name_value"| cut -d'"' -f2 | sed 's/\\n/\n/g' | sed 's/\*.//g'| sed -r 's/([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})//g' |sort | uniq > `pwd`/$req\_domain.txt
 			 rm req.txt
 			 echo ""
-			 cat output/domain.$req.txt
+			 cat `pwd`/$req\_domain.txt
 			 echo ""
-			 echo -e "\e[32m[+]\e[0m Total Save will be \e[31m"$(cat output/domain.$req.txt | wc -l)"\e[0m Domain only"
-			 echo -e "\e[32m[+]\e[0m Output saved in output/domain.$req.txt"
+			 echo -e "\e[32m[+]\e[0m Total Save will be \e[31m"$(cat `pwd`/$req\_domain.txt | wc -l)"\e[0m Domain only"
+			 echo -e "\e[32m[+]\e[0m Output saved in `pwd`/$req""_domain.txt"
 }		 
     # Request the Search with Organization Name
 	Organization() {
 	requestsearch="$(curl -s "https://crt.sh?q=$req&output=json")"
 		 
 			 echo $requestsearch > req.txt
-			 cat req.txt | jq ".[].common_name"| cut -d'"' -f2 | sed 's/\\n/\n/g' | sed 's/\*.//g'| sed -r 's/([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})//g' |sort | uniq > output/org.$req.txt
+			 cat req.txt | jq ".[].common_name"| cut -d'"' -f2 | sed 's/\\n/\n/g' | sed 's/\*.//g'| sed -r 's/([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})//g' |sort | uniq > `pwd`/$req\_org.txt
 			 rm req.txt
 			 echo ""
-			 cat output/org.$req.txt
+			 cat `pwd`/$req\_org.txt
 			 echo ""
-			 echo -e "\e[32m[+]\e[0m Total Save will be \e[31m"$(cat output/org.$req.txt | wc -l)"\e[0m Domain only"
-			 echo -e "\e[32m[+]\e[0m Output saved in output/org.$req.txt"
+			 echo -e "\e[32m[+]\e[0m Total Save will be \e[31m"$(cat `pwd`/$req\_org.txt | wc -l)"\e[0m Domain only"
+			 echo -e "\e[32m[+]\e[0m Output saved in `pwd`/$req""_org.txt"
 }	
 
 if [ -z $1 ]
